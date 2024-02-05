300b63b7fc585f4ebad03a00e900e6df501db350
Sebas Lopez <SEBASSRYO08@GMAIL.COM>
2024-02-05 13:06:47 +0100

    Ej.1T04

---
+++ TareasGithub/src/Ejercicio1T04.java
@@ -0,0 +1,16 @@
+
+public class Ejercicio1T04 {
+
+	public static void main(String[] args) {
+		// Declaración de variables
+		int valorN = 3;
+		int valorA = 7;
+
+		// Operaciones aritméticas y mostrar resultados en pantalla
+		System.out.println("Suma: " + (valorA + valorN) + 
+				"\nResta: " + (valorA - valorN) + 
+				"\nMultiplicación: " + (valorN * valorA) + 
+				"\nDivisión: " + (valorN / valorA) + 
+				"\nMódulo: " + (valorN % valorA));
+	}
+}
