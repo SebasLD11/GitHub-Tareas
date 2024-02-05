276893f17df5f94b827902ab8e9ee6b48ec45795
Sebas Lopez <SEBASSRYO08@GMAIL.COM>
2024-02-05 14:02:59 +0100

    EjerciciosOperadores

---
+++ TareasGithub/src/Ejercicio2T04.java
@@ -0,0 +1,35 @@
+
+public class Ejercicio2T04 {
+
+	public static void main(String[] args) {
+		// Declaración de variables
+		 int x = 3;
+		 int y = 9;
+		 double n = 5;
+		 double	m = 7;
+		
+		 // Operaciones aritmétricas y mostrar resultados en pantalla
+		 System.out.println("Suma: " + (x + y) +
+		 		"\nResta: " + (x - y) +
+		 		"\nMultiplicación: " + (x * y) +
+		 		"\nDivisión: " + (x / y) +
+		 		"\nResto: " + (x % y));
+		 System.out.println("Suma: " + (n + m) +
+				"\nResta: " + (n + m) +
+		 		"\nMultiplicación: " + (n * m) +
+		 		"\nDivisión: " + (n / m) +
+		 		"\nResto: " + (n % m));
+		 System.out.println("Suma: " + (x + n) +
+				 "\nDivisión: " + (y / m) +
+				 "\nResto: " + (y % m));
+		 System.out.println("Multiplicación: " + (x * 2));
+		 System.out.println("Multiplicación: " + (y * 2));		
+		 System.out.println("Multiplicación: " + (n * 2));		
+		 System.out.println("Multiplicación: " + (m * 2));
+		 
+		 System.out.println("Suma: " + (x + y + n + m));
+		 System.out.println("Multiplicación: " + (x * y * n * m));
+		 	 		
+	}
+
+}
