814ac058fa7147983edee962ea34a2a377f47c25
Sebas Lopez <SEBASSRYO08@GMAIL.COM>
2024-02-07 14:01:15 +0100

    Update EjercicioHorasT05.java

--- TareasGithub/src/EjercicioHorasT05.java
+++ TareasGithub/src/EjercicioHorasT05.java
@@ -4,7 +4,7 @@ public class EjercicioHorasT05 {
 	public static void main(String[] args) {
 		// Ejercicio horas del día
 		
-		int hora = 21;
+		int hora = 21	;
 		if (hora<=11) {
 			System.out.println("Buenos días");	
 		} else if(hora<=19) {
