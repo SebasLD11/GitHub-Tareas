03b9b13795d6d6c8d4ca5e0386e66fd728a7d1bc
Sebas Lopez <SEBASSRYO08@GMAIL.COM>
2024-02-08 13:59:30 +0100

    Update Ejercicio2VariablesT05.java

--- TareasGithub/src/Ejercicio2VariablesT05.java
+++ TareasGithub/src/Ejercicio2VariablesT05.java
@@ -1,8 +1,20 @@
 
 public class Ejercicio2VariablesT05 {
 
+
 	public static void main(String[] args) {
+		//Declaración de variables
 		
+		int num = 3;
+		int num1= 9;
 		
+		//Cual es mayor?
+		
+		if 	(num>num1); {
+			System.out.println("El 3 es menor");
+		} else if  ((num1>num) && (num1<num));
+	} else;{
+			System.out.println("El 9 es mayor");
+	    }
 	}
-}
+
