2d4c0adcfeb70093506080f013d815ed049bec61
Sebas Lopez <SEBASSRYO08@GMAIL.COM>
2024-02-14 13:51:33 +0100

    ejercicioT05

---
+++ TareasGithub/src/ud5JavaBasics/Ej07TA5.java
@@ -0,0 +1,20 @@
+package ud5JavaBasics;
+
+public class Ej07TA5 {
+	
+	public static void main(String[] args) {
+		
+		int i=1, j=100;
+		//Incremento ir de 1 a 100
+		while (i<=100) {
+			System.out.println(i);
+			i++;
+		}
+		//Decremento ir de 100 a 1
+		while (j>=1) {
+			System.out.println(j);
+			j--;
+		}
+	}
+
+}
