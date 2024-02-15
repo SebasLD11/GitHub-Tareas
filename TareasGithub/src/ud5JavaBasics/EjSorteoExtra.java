9263aaf5a5c4a0780dcc108f2bc339cf8634d9c7
Sebas Lopez <SEBASSRYO08@GMAIL.COM>
2024-02-15 10:59:49 +0100

    Update EjSorteoExtra.java

--- TareasGithub/src/ud5JavaBasics/EjSorteoExtra.java
+++ TareasGithub/src/ud5JavaBasics/EjSorteoExtra.java
@@ -2,12 +2,14 @@ package ud5JavaBasics;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class EjSorteoExtra {
 
 	public static void main(String[] args) {
-		// pedir 3 strings por pantalla
-		//realizar recorrido del array y mostrar 3 participantes en lista
-		//con un método Math.random
+		//pedir strings por pantalla
+		//realizar recorrido del array y mostrar participantes en lista
+		//con método Math.random escoger ganador/a
 		//rango del 0 al 2(lenght-1) te arroje un ganador.
 		
 		Scanner scanner = new Scanner(System.in);
@@ -16,20 +18,29 @@ public class EjSorteoExtra {
         // Nombres de los participantes
         
         for (int i = 0; i < 10; i++) {
-            System.out.print("Ingrese el nombre del participante " + (i + 1) + ": ");
+        	
+        	System.out.print("Ingrese el nombre del participante " + (i + 1) + ": ");
             participantes[i] = scanner.nextLine();
+            participantes[i] = JOptionPane.showInputDialog("Participante " + (i + 1) );
         }
-        // Mostrar los participantes
         
-        System.out.println("\nLista de participantes:");
+        // Mostrar los participantes con el "for each"
+        
+        	System.out.println("\nLista de participantes:");
+        	JOptionPane.showMessageDialog(null, "\nLista de participantes:");
+        	
         for (String participante : participantes) {
             System.out.println("- " + participante);
+            JOptionPane.showMessageDialog(null, "* " + participante);
+            
         }
-     // Sorteo
+        
+        // Sorteo
         
         int indiceGanador = (int) (Math.random() * participantes.length);
         String ganador = participantes[indiceGanador];
         System.out.println("\n¡El ganador del sorteo es: " + ganador + "!");
+        JOptionPane.showMessageDialog(null,("\n¡El ganador del sorteo es: " + ganador + "!"));
     }
 }
 	
