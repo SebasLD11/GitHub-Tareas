a073a4be8a084705b4e6bad60526d53f1045e0f1
Sebas Lopez <SEBASSRYO08@GMAIL.COM>
2024-05-20 13:11:49 +0200

    UD20MavenProject

---
+++ C4.Tech/src/test/java/C4/TemarioTech/C4/Tech/AppTest.java
@@ -0,0 +1,20 @@
+package C4.TemarioTech.C4.Tech;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ TareasGithub/src/UD18ConexionJava/Ejercicio3Insert.java
@@ -0,0 +1,5 @@
+package UD18ConexionJava;
+
+public class Ejercicio3Insert {
+
+}
