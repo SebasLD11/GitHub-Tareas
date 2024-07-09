ef91e2347f08a21331a2a1d92a2661046b35583a
Sebas Lopez <SEBASSRYO08@GMAIL.COM>
2024-07-10 01:02:29 +0200

    Archivos añadidos FrontEnd

--- TFT-Services/FRONTEND/src/app/components/body/body.component.ts
+++ TFT-Services/FRONTEND/src/app/components/body/body.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
+  templateUrl: './body.component.html',
   styleUrls: ['./home.component.css']
 })
-export class FooterComponent { }
+export class BodyComponent { }
--- TFT-Services/FRONTEND/src/app/components/header/header.component.ts
+++ TFT-Services/FRONTEND/src/app/components/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
+  templateUrl: './header.component.html',
   styleUrls: ['./home.component.css']
 })
-export class FooterComponent { }
+export class HeaderComponent { }
