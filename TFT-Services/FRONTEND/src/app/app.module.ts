df1756293fc2d876368e60f358c099ed1b057a62
Sebas Lopez <SEBASSRYO08@GMAIL.COM>
2024-07-11 14:05:29 +0200

    Modificaciones final Clase

--- TFT-Services/FRONTEND/src/app/app.module.ts
+++ TFT-Services/FRONTEND/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './pages/body/body.component';
+import { BodyComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
