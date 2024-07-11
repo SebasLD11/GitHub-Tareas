32f3fd697e530119b6719a97e4da4dc477df6799
Sebas Lopez <SEBASSRYO08@GMAIL.COM>
2024-07-11 03:23:12 +0200

    continuar con modificaciones del front
    faltan pocos archivos por conectar para poder hacer una prueba

---
+++ TFT-Services/FRONTEND/src/polyfills.ts
@@ -0,0 +1,45 @@
+/**
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
+ *
+ * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
+ * automatically update themselves. This includes recent versions of Safari, Chrome (including
+ * Opera), Edge on the desktop, and iOS and Chrome on mobile.
+ *
+ * Learn more in https://angular.io/guide/browser-support
+ */
+
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+// IE9, IE10 and IE11 requires all of the following polyfills.
+// import 'core-js/es6/symbol';
+// import 'core-js/es6/object';
+// import 'core-js/es6/function';
+// import 'core-js/es6/parse-int';
+// import 'core-js/es6/parse-float';
+// import 'core-js/es6/number';
+// import 'core-js/es6/math';
+// import 'core-js/es6/string';
+// import 'core-js/es6/date';
+// import 'core-js/es6/array';
+// import 'core-js/es6/regexp';
+// import 'core-js/es6/map';
+// import 'core-js/es6/set';
+
+/**
+ * If the application will be indexed by Google Search, the following is required.
+ * Googlebot uses a renderer based on Chrome 41.
+ * https://developers.google.com/search/docs/guides/rendering
+ */
+// import 'core-js/es6/array';
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
+
+/***************************************************************************************************
+ * Zone JS is required by Angular itself.
+ */
+import 'zone.js'; // Included with Angular CLI.
