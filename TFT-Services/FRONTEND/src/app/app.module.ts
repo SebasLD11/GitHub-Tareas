42473ab8c55c679ee3f4288040f7b5217e604069
Sebas Lopez <SEBASSRYO08@GMAIL.COM>
2024-07-13 01:14:10 +0200

    ultimas modificaciones del dia en el front y conexion
    continuar mañana con la conexion para que la compilación vaya bien y podamos centrarnos en la parte visual de la pagina para presentar maqueta en la presentacion

--- TFT-Services/FRONTEND/src/app/app.module.ts
+++ TFT-Services/FRONTEND/src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { UserService } from './services/user.service';
-import { AuthService } from './services/auth.service';
-import { routes } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
-import { AuthComponent } from './auth/auth.component';  // Verifica esta ruta
+import { AuthComponent } from './auth/auth.component';  // Asegúrate de que esta ruta es correcta
 import { ServicesComponent } from './services/services.component';
-import { ProfileComponent } from './pages/profile/profile.component';  // Verifica esta ruta
+import { ProfileComponent } from './pages/profile/profile.component';  // Asegúrate de que esta ruta es correcta
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AdminComponent } from './admin/admin.component';
+
+import { AuthService } from './services/auth.service';
+import { UserService } from './services/user.service';
+import { AdminService } from './admin/admin.service';
+
+import { routes } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -33,11 +36,15 @@ import { AdminComponent } from './admin/admin.component';
   ],
   imports: [
     BrowserModule,
-    HttpClient,
     FormsModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [AuthService, UserService],
+  providers: [ 
+    provideHttpClient(withInterceptorsFromDi()),
+    AuthService,
+    UserService, 
+    AdminService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
